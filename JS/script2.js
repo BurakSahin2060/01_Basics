bf20669def966f086f47a1d40add12a30854dca0
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2023-12-07 11:23:10 +0100

    END OF LESSON

--- JS/script2.js
+++ JS/script2.js
@@ -6,19 +6,37 @@ let a = 7;
 let b = 8;
 let c = 10;
 
-// Berechne folgendes und gib das Eregebnis über console.log aus
+// Berechne folgendes und gib das Ergebnis über console.log aus
 
-let result = a + b + c;
-console.log("Das Ergebnis ist " + result);
 // a + b + c
+let result1 = a + b + c;
+console.log("Das Ergebnis ist " + result1);
+
 // (a + b) / c
-// c -a + b
+let result2 = (a + b) / c;
+console.log("Das Ergebnis ist " + result2);
+
+// c - a + b
+let result3 = c - a + b;
+console.log("Das Ergebnis ist " + result3);
+
 // a + b + c * 3
+let result4 = a + b + c * 3;
+console.log("Das Ergebnis ist " + result4);
+
 // (a + b + c)/4
+let result5 = (a + b + c) / 4;
+console.log("Das Ergebnis ist " + result5);
+
 
 // Ändere die Variablen auf folgende Werte
 // a = 3
+a = 3;
+
 // b = a + 3
+b = a + 3;
+
 // c = c -3
+c = c - 3;
 
 // gib die neuen Werte der Variablen aus
