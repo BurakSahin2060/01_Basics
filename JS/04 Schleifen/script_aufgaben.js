dddc2691db516d891ae2b4237987df32b9525fae
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2023-12-21 10:25:49 +0100

    04 Schleifen I Aufgabe DONE

--- JS/04 Schleifen/script_aufgaben.js
+++ JS/04 Schleifen/script_aufgaben.js
@@ -1,17 +1,35 @@
 // Zähle von 7 bis 45
-
+for (let count = 7; count < 45; count++) {
+    console.log(count);
+}
 
 // Zähle von 9 bis 99
-
+for (let index = 9; index < 100; index++) {
+    console.log(index);
+}
 
 // Zähle rückwärts von 10 bis 0
-
+for (let numbers = 10; numbers >= 0; numbers--) {
+    console.log(numbers);
+}
 
 // Zähle rückwärts von 1234 bis 1207
-
+for (let ziffern = 1234; ziffern >= 1207; ziffern--) {
+    console.log(ziffern);
+}
 
 // Gib Deinen Namen genau 8 mal aus
-
+for (let name = 1; name <=8; name++) {
+    console.log(name + " Burak");
+}
 
 // Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let zahlen = 0; zahlen <= 10; zahlen++) {
+    console.log(zahlen);
+}
 
+console.log("Burak");
+
+for (let variablen = 10; variablen >= 0; variablen--) {
+    console.log(variablen);
+}
