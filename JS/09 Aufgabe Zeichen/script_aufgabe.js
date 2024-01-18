c758eb36961a942e2a5616b3eba68239b589995e
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-01-18 11:23:55 +0100

    09 Aufgabe Zeichen l END OF LESSON

---
+++ JS/09 Aufgabe Zeichen/script_aufgabe.js
@@ -0,0 +1,48 @@
+// Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert (Rechteckbeispiel adaptieren): 
+// X X X X 
+// O O O O 
+// X X X X 
+// O O O O 
+
+
+for (let index = 1; index < 5; index++) {
+    console.log("x");
+    if (index%5 == 0) {
+        console.log("");
+    }
+}
+
+
+for (let index = 1; index < 5; index++) {
+    console.log("o");
+    if (index%5 == 0) {
+        console.log("");
+    }
+}
+
+
+for (let index = 1; index < 5; index++) {
+    console.log("x");
+    if (index%5 == 0) {
+        console.log("");
+    }
+}
+
+
+for (let index = 1; index < 5; index++) {
+    console.log("o");
+    if (index%5 == 0) {
+        console.log("");
+    }
+}
+
+// Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler 
+// dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.)
+
+
+/* Versuche mit einer for/while Schleife den Anfangsbuchstaben deines Namens auszugeben, z.B.: 
+XXXXX X 
+X    X X   X 
+X            X
+X            X */
+
