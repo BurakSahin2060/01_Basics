b5215642f10d8769c6d176b28d75f261a19694f8
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-01-18 10:20:58 +0100

    08 Switch Statement l DONE

---
+++ JS/08 Switch Statement/script.js
@@ -0,0 +1,31 @@
+// Erstelle eine Zufallszahl zwischen 5 und 10
+let randomNumber = Math.random() * 6 + 5;
+let random = Math.floor(randomNumber);
+console.log(random);
+
+// Wenn der Wert 10 ist gib aus Ten
+if (random == 10) {
+    console.log("Ten");
+}
+
+// Wenn der Wert 9 ist gib aus Nine
+if (random == 9) {
+    console.log("Nine");
+}
+
+// Wenn der Wert 8 ist gib aus Eight
+if (random == 8) {
+    console.log("Eight");
+}
+
+if (random == 7) {
+    console.log("Seven");
+}
+
+if (random == 6) {
+    console.log("Six");
+}
+
+if (random == 5) {
+    console.log("Five");
+}
