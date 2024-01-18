d0ef11e3b35a1b09f3b8e4c84c4ad64b0875eb9a
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-01-18 10:42:30 +0100

    09 Aufgabe Zeichen l VIDEO DONE

---
+++ JS/09 Aufgabe Zeichen/script.js
@@ -0,0 +1,15 @@
+let a = 1999;
+let b = 2;
+
+// if (a%8 == 0) {
+//    console.log("Number is even");
+// } else {
+//    console.log("Number is odd");
+// }
+
+for (let index = 1; index < 500; index++) {
+    console.log("x");
+    if (index%5 == 0) {
+        console.log("");
+    }
+}
