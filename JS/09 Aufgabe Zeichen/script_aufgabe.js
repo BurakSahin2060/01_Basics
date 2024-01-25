a4dd930ec65cb1725385d805ad1a1bdddb1cabe4
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-01-25 11:21:07 +0100

    09 Aufgabe Zeichen | Aufgabe 1 & 3 DONE

--- JS/09 Aufgabe Zeichen/script_aufgabe.js
+++ JS/09 Aufgabe Zeichen/script_aufgabe.js
@@ -1,48 +1,123 @@
-// Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert (Rechteckbeispiel adaptieren): 
-// X X X X 
-// O O O O 
-// X X X X 
-// O O O O 
+// Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert (Rechteckbeispiel adaptieren):
+// X X X X
+// O O O O
+// X X X X
+// O O O O
 
 
-for (let index = 1; index < 5; index++) {
-    console.log("x");
-    if (index%5 == 0) {
-        console.log("");
-    }
-}
-
+let output = "";
 
 for (let index = 1; index < 5; index++) {
-    console.log("o");
-    if (index%5 == 0) {
-        console.log("");
-    }
+  output += "x";
+  if (index%4==0){
+    console.log(output);
+    output=""
+  }
 }
-
 
 for (let index = 1; index < 5; index++) {
-    console.log("x");
-    if (index%5 == 0) {
-        console.log("");
+    output += "o";
+    if (index%4==0){
+      console.log(output);
+      output=""
     }
-}
+  }
 
 
-for (let index = 1; index < 5; index++) {
-    console.log("o");
-    if (index%5 == 0) {
-        console.log("");
+  for (let index = 1; index < 5; index++) {
+    output += "x";
+    if (index%4==0){
+      console.log(output);
+      output=""
     }
-}
+  }
+  
 
-// Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler 
+  for (let index = 1; index < 5; index++) {
+    output += "o";
+    if (index%4==0){
+      console.log(output);
+      output=""
+    }
+  }
+
+  
+// Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler
 // dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.)
 
 
-/* Versuche mit einer for/while Schleife den Anfangsbuchstaben deines Namens auszugeben, z.B.: 
-XXXXX X 
-X    X X   X 
-X            X
-X            X */
 
+
+
+/* Versuche mit einer for/while Schleife den Anfangsbuchstaben deines Namens auszugeben, z.B.: 
+xxxxx
+x    x
+x    x
+xxxxx
+x    x
+x    x
+xxxxx                             */
+
+let output2 = "";
+console.log(output2);
+
+for (let index = 1; index < 6; index++) {
+    output2 += "x";
+    if (index%5==0){
+      console.log(output2);
+      output2=""
+    }
+  }
+
+  for (let index = 1; index < 2; index++) {
+    output2 += "x    x";
+    if (index%1==0){
+      console.log(output2);
+      output2=""
+    }
+  }
+
+  for (let index = 1; index < 2; index++) {
+    output2 += "x    x";
+    if (index%1==0){
+      console.log(output2);
+      output2=""
+    }
+  }
+
+  for (let index = 1; index < 6; index++) {
+    output2 += "x";
+    if (index%5==0){
+      console.log(output2);
+      output2=""
+    }
+  }
+
+  for (let index = 1; index < 2; index++) {
+    output2 += "x    x";
+    if (index%1==0){
+      console.log(output2);
+      output2=""
+    }
+  }
+
+  for (let index = 1; index < 2; index++) {
+    output2 += "x    x";
+    if (index%1==0){
+      console.log(output2);
+      output2=""
+    }
+  }
+
+  for (let index = 1; index < 6; index++) {
+    output2 += "x";
+    if (index%5==0){
+      console.log(output2);
+      output2=""
+    }
+  }
+
+
+
+
+ 
