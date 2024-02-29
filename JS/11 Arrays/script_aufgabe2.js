387c467dd0bcd8b30ace77216b73669a2d21eee9
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-02-29 10:26:50 +0100

    11 Arrays | Aufgabe 2 Done

---
+++ JS/11 Arrays/script_aufgabe2.js
@@ -0,0 +1,11 @@
+// Erstelle ein Array mit folgenden Strings: "Susi", "Paula", "Hans"
+let arr = ["Susi", "Paula", "Hans"];
+
+// Gib folgenden Satz aus: "Meine Freunde sind Susi, Paula und Hans
+console.log("Meine Freunde sind " + arr[0] + ", " + arr[1] + " und " +arr[2]);
+
+// Füge "Sepp" dazu. 
+arr.push("Sepp");
+
+// Nun soll die ausgabe folgendermaßen lauten: "Meine Freunde sind Susi, Paula, Hans und Sepp"
+console.log("Meine Freunde sind " + arr[0] + ", " + arr[1] + ", " + arr[2] + " und " + arr[3])
