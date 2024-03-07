aad709d1da0c82e14d6729d1c963c4517b452632
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-03-07 11:26:18 +0100

    END OF LESSON

---
+++ JS/11 Arrays/Aufgabe 3/script.js
@@ -0,0 +1,14 @@
+// Write a program that prints the temperature closest to 0 among input data. 
+// If two numbers are equally close to zero, positive integer has to be considered closest to zero 
+// (for instance, if the temperatures are -5 and 5, then display 5).
+
+// Input
+// 5
+// 1 -2 -8 4 5
+// Output
+// 1
+
+let temperatures = [1, -2, -8, 4, 5];
+let arr = temperatures.split("");
+
+console.log(arr);
