a57feb4ea84ecb9807c6d4c8bcf0c3f54d3aa379
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-05-16 10:35:27 +0200

    01 Zugriff JS HTML | Implementierung - Mini Rechner | DONE

--- HTML_JS/01 Zugriff JS HTML/script mini rechner.js
+++ HTML_JS/01 Zugriff JS HTML/script mini rechner.js
@@ -1,10 +1,39 @@
-function buttonClicked() {
-    console.log("clicked");
-    let num1 = document.getElementById("number1").value;
-    let num2 = document.getElementById("number2").value;
+function buttonAddition() {
+  let add1 = document.getElementById("add1").value;
+  let add2 = document.getElementById("add2").value;
 
-    let result = parseFloat(num1) + parseFloat(num2);
+  let resultAddition = parseFloat(add1) + parseFloat(add2);
 
-    console.log(result);
+  console.log(resultAddition);
+  document.querySelector(".result").innerHTML = resultAddition;
 }
 
+function buttonSubtraction() {
+  let subtract1 = document.getElementById("subtract1").value;
+  let subtract2 = document.getElementById("subtract2").value;
+
+  let resultSubtraction = parseFloat(subtract1) - parseFloat(subtract2);
+
+  console.log(resultSubtraction);
+  document.querySelector(".result").innerHTML = resultSubtraction;
+}
+
+function buttonMultiplication() {
+  let multiple1 = document.getElementById("multiple1").value;
+  let multiple2 = document.getElementById("multiple2").value;
+
+  let resultMultiplication = parseFloat(multiple1) * parseFloat(multiple2);
+
+  console.log(resultMultiplication);
+  document.querySelector(".result").innerHTML = resultMultiplication;
+}
+
+function buttonDivision() {
+  let divison1 = document.getElementById("divison1").value;
+  let divison2 = document.getElementById("divison2").value;
+
+  let resultDivision = parseFloat(divison1) / parseFloat(divison2);
+
+  console.log(resultDivision);
+  document.querySelector(".result").innerHTML = resultDivision;
+}
