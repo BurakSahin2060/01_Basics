999f86cf5cbe2c081e388b09a583404c4a8cc002
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-06-20 10:25:42 +0200

    Übungen zu den Arrays | DONE

--- JS/Übung 2aWI_13.06.2024/script.js
+++ JS/Übung 2aWI_13.06.2024/script.js
@@ -1,55 +1,50 @@
 // Multipliziere jede 3te Zahl und dividiere durch die Anzahl der geraden Zahlen
 // (5 * 4 * 3) / 3
-
 const data1 = "5,2,3,4,1,2,3,5";
 
-const numbers = data1.split(",").map(Number);
+const number1 = data1.split(",");
 
-let geradeZahlen = 0;
-let endproduct = 1;
+let result1 = 1;
+let straightNumbers = 0;
 
-for (let index = 0; index < numbers.length; index++) {
-  if ((index + 1) % 3 === 0) {
-    endproduct *= numbers[index];
-  }
-
-  if (numbers[index] % 2 === 0) {
-    geradeZahlen++;
+for (let i = 0; i < number1.length; i += 3) {
+  result1 *= parseInt(number1[i]);
+  if (parseInt(number1[i]) % 2 == 0) {
+    straightNumbers += 1;
   }
 }
 
-const result = endproduct / geradeZahlen;
-
-console.log(result);
+console.log(result1 / straightNumbers);
 
 // Berechne die Summe der geraden durch die Summe der ungeraden Zalen
-
 const data2 = "1,2,3,4,5,6,7,8,9,10,11,12";
 
 const numbers2 = data2.split(",");
+let geraden = 0;
+let ungeraden = 0;
 
-let sumGrd = 0;
-let sumUngrd = 0;
-
-for (let index = 0; index < numbers.length; index++) {
-  if (numbers[index] % 2 === 0) {
-    sumGrd += numbers[index];
+for (let index = 0; index < numbers2.length; index++) {
+  if (parseInt(numbers2[index]) % 2 == 0) {
+    geraden += parseInt(numbers2[index]);
   } else {
-    sumUngrd += numbers[index];
+    ungeraden += parseInt(numbers2[index]);
   }
 }
 
-const result2 = sumGrd / sumUngrd;
-
-console.log(result);
+console.log("Summe der geraden Zahlen: " + geraden);
+console.log("Summe der ungeraden Zahlen: " + ungeraden);
 
 // Gib nur die Wörter aus, die ein e enthalten
-
 const data3 = "Hans ist ein netter Kerl!";
 
-let arr3 = data3.split(" ");
+const words = data3.split(" ");
+let result3 = " ";
 
-console.log(arr3);
-
-if (condition) {
+for (let i = 0; i < words.length; i++) {
+  if (words[i].includes("e")) {
+    result3 += words[i];
+    result3 += " ";
+  }
 }
+
+console.log("Wörter die ein e enthalten: " + result3);
---
+++ JS/Übung 2aWI_13.06.2024/scripty.js
@@ -0,0 +1,13 @@
+// Multipliziere alle ungeraden Zahlen miteinander 
+// und dividiere durch die Anzahl der geraden Zahlen 
+// (2*4*2...)/6 
+const data1 = "5,2,3,4,1,2,3,5,10,-11"; 
+
+// Zähle die negativnen Zahlen, die positiven Zahlen und die Anzahl von 0 
+// Gib die prozentuelle Verteilung an 
+const data2 = "1,2,3,4,0,-2,0,-1,5,6,7,8,9,10,11,12"; 
+
+
+// gib nur die Wörter aus, die ein e aber kein l enthalten 
+// ein, netter 
+const data3 = "Hans ist ein netter Kerl!";
